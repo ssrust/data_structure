ed8a4f71aef11e3a20bdc844daeaeab9ca4c21fe
Rootless Han <eddie030218@gmail.com>
2022-05-30 20:30:49 +0900

    Separate tests module from main file

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,1 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn placeholder() {
-        assert!({"Make sure to delete this before start a real project!"; false})
-    }
-}
+mod tests;
---
+++ src/tests.rs
@@ -0,0 +1,6 @@
+#![cfg(test)]
+
+#[test]
+fn placeholder() {
+    assert!({"Make sure to delete this before start a real project!"; false})
+}
